660999328c2e51e2c154d9a8c79554e47de76085
uhyo <uhyo@uhy.ooo>
2019-01-05 19:46:27 +0900

    add Cache-Control

--- server/micro.config.js
+++ server/micro.config.js
@@ -1,16 +1,24 @@
-const dispatch = require('micro-route/dispatch')
-const { Nuxt, Builder } = require('nuxt')
+const dispatch = require('micro-route/dispatch');
+const { Nuxt, Builder } = require('nuxt');
 
 // Require nuxt config
-const config = require('../nuxt.config.js')
+const config = require('../nuxt.config.js');
 // Create a new nuxt instance
-const nuxt = new Nuxt(config)
+const nuxt = new Nuxt(config);
 // Enable live build & reloading on dev
-if (nuxt.options.dev) {
-  new Builder(nuxt).build()
+const isDev = nuxt.options.dev;
+if (isDev) {
+  new Builder(nuxt).build();
 }
 
 module.exports = async (req, res) => {
-  await dispatch()
-    .dispatch('*', ['GET'], (req, res) => nuxt.render(req, res))(req, res)
-}
+  await dispatch().dispatch('*', ['GET'], (req, res) => {
+    if (!isDev) {
+      res.setHeader(
+        'Cache-Control',
+        'public, max-age=86400, s-max-age=2592000'
+      );
+    }
+    return nuxt.render(req, res);
+  })(req, res);
+};
