8544835f10ed513921a9b2b9bdfe86a0ce22d881
uhyo <uhyo@uhy.ooo>
2019-02-28 22:44:00 +0900

    implement cache-control

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ import staticServer from '@fly/static';
 import { getGengouData, canonical } from './logic/kanji';
 import { gengouIdString } from './logic/gengou-code';
 
+// cache setting
+const cache = {
+  // cache for pages
+  page: 'public, max-age=86400, s-max-age=2592000',
+  assets: 'public, max-age=2592000, immutable'
+};
+
 // fly application
 const staticRoute = staticServer({
   root: '/dist'
 });
-fly.http.respondWith(request => {
+fly.http.respondWith(async request => {
   const url = new URL(request.url);
   const pathname = url.pathname;
-  console.log(pathname);
   if (/\/[0-9a-f]{8}$/i.test(pathname)) {
     // detected a URL of gengou page.
     return generateGengouPage(pathname.slice(1));
@@ -17,7 +23,12 @@ fly.http.respondWith(request => {
     url.pathname += '/';
   }
   request.url = url;
-  return staticRoute(request);
+  const res = await staticRoute(request);
+  res.headers.set(
+    'cache-control',
+    url.pathname.endsWith('/') ? cache.page : cache.assets
+  );
+  return res;
 });
 
 // generate gengou page.
@@ -45,5 +56,6 @@ async function generateGengouPage(gengouCodeStr) {
     (_, name) => replaceDict[name]
   );
   const newResponse = new Response(newBody, templateResp);
+  newResponse.headers.set('cache-control', cache.page);
   return newResponse;
 }
